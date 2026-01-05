3fccc976e81939ec28af06482e90032134b6ca3a
Ahishakiye800 <sun.ahishakiye@aims-cameroon.org>
2026-01-05 09:40:51 +0100

    second commit 43

--- server.js
+++ server.js
@@ -93,3 +93,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+if (!process.env.JWT_SECRET) {
+  console.error("❌ FATAL ERROR: JWT_SECRET is not defined.");
+  process.exit(1); 
+}
